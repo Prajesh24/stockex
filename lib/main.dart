2ff9e943590a4b90c01953407c659857505d3eb5
Prajesh24 <149761502+Prajesh24@users.noreply.github.com>
2026-01-02 21:38:55 +0545

    modifying main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:stockex/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:stockex/app/app.dart';
+import 'package:stockex/core/services/hive/hive_service.dart';
+import 'package:stockex/features/auth/data/model/auth_hive_model.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final hiveService = HiveService();
+  await hiveService.init();
+
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
+
