9593000f64d030fbbd103d7431a1c119fe92307a
Prajesh24 <149761502+Prajesh24@users.noreply.github.com>
2026-01-02 21:29:33 +0545

    adding hive table constant

---
+++ lib/core/constants/hive_table_constant.dart
@@ -0,0 +1,9 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  static const String dbName = 'db_table';
+
+  //auth
+  static const int authTypeId = 0;
+  static const String authTable = 'auth_table';
+}
