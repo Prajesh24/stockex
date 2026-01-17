4b801f3cc74f5dec09f38e40bd3b1fcdef5c2397
Prajesh24 <149761502+Prajesh24@users.noreply.github.com>
2026-01-17 07:22:07 +0545

    adding app_client

---
+++ lib/core/api/app_client.dart
@@ -0,0 +1,173 @@
+import 'package:dio/dio.dart';
+import 'package:dio_smart_retry/dio_smart_retry.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+import 'package:stockex/core/api/api_endpoints.dart';
+
+// 🔹 Provider
+final apiClientProvider = Provider<ApiClient>((ref) {
+  return ApiClient();
+});
+
+class ApiClient {
+  late final Dio _dio;
+
+  ApiClient() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: ApiEndpoints.baseUrl,
+        connectTimeout: ApiEndpoints.connectionTimeout,
+        receiveTimeout: ApiEndpoints.receiveTimeout,
+        headers: const {
+          'Content-Type': "application/json",
+          'Accept': "application/json",
+        },
+      ),
+    );
+
+    // 🔐 Auth interceptor
+    _dio.interceptors.add(_AuthInterceptor());
+
+    // 🔁 Retry on network failure
+    _dio.interceptors.add(
+      RetryInterceptor(
+        dio: _dio,
+        retries: 3,
+        retryDelays: const [
+          Duration(seconds: 1),
+          Duration(seconds: 2),
+          Duration(seconds: 3),
+        ],
+        retryEvaluator: (error, attempt) {
+          return error.type == DioExceptionType.connectionTimeout ||
+              error.type == DioExceptionType.sendTimeout ||
+              error.type == DioExceptionType.receiveTimeout ||
+              error.type == DioExceptionType.connectionError;
+        },
+      ),
+    );
+
+    // 🐞 Logger (Debug only)
+    if (kDebugMode) {
+      _dio.interceptors.add(
+        PrettyDioLogger(
+          requestHeader: true,
+          requestBody: true,
+          responseBody: true,
+          responseHeader: true,
+          error: true,
+          compact: true,
+        ),
+      );
+    }
+  }
+
+  Dio get dio => _dio;
+
+  // GET
+  Future<Response> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return _dio.get(path,
+        queryParameters: queryParameters, options: options);
+  }
+
+  // POST
+  Future<Response> post(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return _dio.post(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  // PUT
+  Future<Response> put(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return _dio.put(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  // DELETE
+  Future<Response> delete(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return _dio.delete(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  // FILE UPLOAD
+  Future<Response> uploadFile(
+    String path, {
+    required FormData formData,
+    Options? options,
+    ProgressCallback? onSendProgress,
+  }) {
+    return _dio.post(
+      path,
+      data: formData,
+      options: options,
+      onSendProgress: onSendProgress,
+    );
+  }
+}
+
+// 🔐 Auth Interceptor
+class _AuthInterceptor extends Interceptor {
+  final _storage = const FlutterSecureStorage();
+  static const String _tokenKey = 'auth_token';
+
+  @override
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    final isAuthEndpoint =
+        options.path == ApiEndpoints.login ||
+        options.path == ApiEndpoints.register;
+
+    if (!isAuthEndpoint) {
+      final token = await _storage.read(key: _tokenKey);
+      if (token != null) {
+        options.headers["Authorization"] = "Bearer $token";
+      }
+    }
+
+    handler.next(options);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    if (err.response?.statusCode == 401) {
+      _storage.delete(key: _tokenKey);
+      // TODO: navigate to login
+    }
+    handler.next(err);
+  }
+}
